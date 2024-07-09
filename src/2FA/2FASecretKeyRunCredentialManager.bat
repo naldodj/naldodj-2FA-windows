b7518bc5eb7d453b50dde50c6664131d47a470ff
Naldo DJ <marinaldo.jesus@gmail.com>
2024-07-09 12:55:12 -0300

    change 2FASecretKeyRunCredentialManager.bat

--- src/2FA/2FASecretKeyRunCredentialManager.bat
+++ src/2FA/2FASecretKeyRunCredentialManager.bat
@@ -1,1 +1,3 @@
-pwsh -executionPolicy bypass -file "C:\tools\2FA\2FASecretKeyGetCredentialManager.ps1" > "C:\2FA\2FASecretKeyRunCredentialManager.log"
+pwsh -executionPolicy bypass -file "C:\tools\2FA\2FASecretKeyGetCredentialManager.ps1" > "C:\2FA\2FASecretKeyRunCredentialManager.log" 2>&1
+
+ 
