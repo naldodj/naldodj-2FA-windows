e337be4fdcea6082a4f503f4706f8da4705cda40
Naldo DJ <marinaldo.jesus@gmail.com>
2024-12-21 19:39:53 -0300

    Update 2FA.bat

--- src/2FA/hb/2FA.bat
+++ src/2FA/hb/2FA.bat
@@ -4,15 +4,6 @@ setlocal enabledelayedexpansion
 rem Diretório base onde as aplicações estão localizadas
 set baseDir=.
 
-rem Verificar se um argumento foi passado
-if not "%~1"=="" (
-    rem Se existir um script em PowerShell a ser executado, executa-o
-    if exist "%1" (
-        rem Executar o comando e redirecionar a saída para o arquivo de log
-        start /b /realtime /min /wait pwsh -WindowStyle Hidden -NoProfileLoadTime -NoProfile -NonInteractive -NoLogo -STA -Login -NoExit -executionPolicy bypass -file %1
-    )
-)
-
 rem Array de aplicações com paths completos
 set apps[0]=%baseDir%\minigui\clock\2FAClockSaver.exe
 set apps[1]=%baseDir%\minigui\lines\2FALines.exe
@@ -23,7 +14,17 @@ rem Gerar um número aleatório entre 0 e 3
 set /a rand=%random% %% 4
 
 rem Executar a aplicação aleatória com o parâmetro /s
-start "" "!apps[%rand%]!" /s /b /realtime /min /wait
-exit
+start "" "!apps[%rand%]!" /s /b /realtime /min
+
+rem Verificar se um argumento foi passado
+if not "%~1"=="" (
+    rem Se existir um script em PowerShell a ser executado, executa-o
+    if exist "%1" (
+        rem Executar o comando e redirecionar a saída para o arquivo de log
+        start /b /realtime /min pwsh -WindowStyle Hidden -NoProfileLoadTime -NoProfile -NonInteractive -NoLogo -STA -Login -executionPolicy bypass -file %1
+    )
+)
 
 endlocal
+
+exit
